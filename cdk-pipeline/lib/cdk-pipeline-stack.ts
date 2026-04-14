d5bed784965872fdc9924038eda3942987ebe366
tm-efosso <ega.fosso@toumoro.com>
2026-04-14 14:18:36 -0400

    configure cdk pipeline

--- cdk-pipeline/lib/cdk-pipeline-stack.ts
+++ cdk-pipeline/lib/cdk-pipeline-stack.ts
@@ -1,16 +1,29 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
+import * as pipelines from 'aws-cdk-lib/pipelines';
+import { PipelineType } from "aws-cdk-lib/aws-codepipeline";
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Access the CommitId of a GitHub source in the synth
+    const source = pipelines.CodePipelineSource.gitHub('egafossojm/CDK', 'main');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkPipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create pipeline with S3 source
+    const pipeline = new pipelines.CodePipeline(this, "Pipeline", {
+      pipelineName: "WorkshopPipeline",
+      pipelineType: PipelineType.V2,
+      synth: new pipelines.CodeBuildStep("SynthStep", {
+        input: source,
+        commands: [
+          "npm ci",
+          "npm run build",
+          "npx cdk synth"
+        ],
+      }),
+    });
   }
 }
